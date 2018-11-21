d9484be00ec841cb6b912be1e1b42be919d6b04b
Jowii <jow7lgamer@gmail.com>
2018-11-21 19:43:56 +0100

    4.1, 4.2, 4.7, 4.8

---
+++ Tema4/T4e7.java
@@ -0,0 +1,24 @@
+
+package t4e7;
+
+public class T4e7 {
+
+    public static void main(String[] args) {
+        
+        /*  En este ejercicio el usuario nos va a proporcionar a traves del teclado
+            el numero de unidades que se venden de los distintos productos con los que
+            trabaja una empresa.
+            Esta empresa trabaja con diez productos distintos cuyos codigos son:
+            10, 23, 30, 47, 55, 65,135,256, 526, 663. El usuario cada vez que se hace 
+            una venta, nos proporcionara el codigo del producto (hay que
+            comprobar que es valido) y el numero de unidades vendidas de ese
+            producto. Al final del dıa, es decir, cuando el usuario nos diga que no
+            desea continuar, nosotros tenemos que visualizar el numero total de
+            unidades vendidas de cada producto y el codigo del producto, todo
+            ello ordenado de menor a mayor por numero de unidades vendidas. */
+        
+        
+            
+    }
+    
+}
---
+++ Tema4/T4e8.java
@@ -0,0 +1,32 @@
+package t4e8;
+import java.util.*;
+import javax.swing.JOptionPane;
+
+public class T4e8 {
+
+    public static void main(String[] args) {
+        /* Crear una matriz de dos dimensiones de 10 x 10. Pediremos que
+        introduzcan el numero de fila y el de columna que vamos a utilizar
+        y el valor que quieren almacenar. Una vez introducidos los datos. Se
+        escribira la suma correspondientes a las filas de la matriz. */
+        
+        int[][] variables = new int[10][10];
+
+        // Llena el array
+        for (int[] row: variables)
+            Arrays.fill(row, 0);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Arrays.toString(variables[i]));
+        }
+        
+        String var1 = JOptionPane.showInputDialog("Introduce el nº de fila:");
+        String var2 = JOptionPane.showInputDialog("Introduce el valor para esa fila:");
+        String var3 = JOptionPane.showInputDialog("Introduce el nº de columna:");
+        String var4 = JOptionPane.showInputDialog("Introduce el valor para esa columna:");
+        
+        
+
+     
+    }
+    
+}
